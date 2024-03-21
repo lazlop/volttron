300fe60a99a742512c97dc333694ca568c89cdb4
Lazlo <LPaul@lbl.gov>
2024-03-21 12:06:32 -0700

    adding hpflex volttron changes

---
+++ update_bacnet_configs.sh
@@ -0,0 +1,15 @@
+dir="configs/devices"
+for f in "$dir"/*; do
+ # echo "$f"
+  config_name="${f##*/}"
+ # echo "$config_name"
+  vctl config store platform.driver devices/wcec/$config_name $f
+done
+
+dir="configs/registry_configs"
+for f in "$dir"/*; do
+    config_name="${f#*/}"
+ # echo "$f"
+ # echo "$config_name"
+  vctl config store platform.driver $config_name $f --csv
+done
